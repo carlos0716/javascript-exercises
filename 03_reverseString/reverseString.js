79f6d19e91ce036fcec44d76190e673553ce7d71
carlos0716 <chulezyc@gmail.com>
2023-07-12 13:34:34 -0600

    solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newArray = Array.from(string);
+  	let reversedArray = newArray.reverse();
+  	let arrayToString = reversedArray.join('');
+    return arrayToString;
 };
-
+console.log(reverseString('fuck'));
 // Do not edit below this line
 module.exports = reverseString;
