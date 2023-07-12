ab730c5a05a9a1864cab2c037e8e46a33cf23eab
carlos0716 <chulezyc@gmail.com>
2023-07-12 13:05:46 -0600

    repeatString solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let word = string;
+    let newString = '';
+    let errorMessage = 'ERROR'
+    if (num < 0) {
+       newString += errorMessage;
+    } else if (num >= 0)
+    for (let i = 0; i < num; i++) {
+       
+      newString += word;
+     
+    }; return newString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
