07318840a2cc51d801345e914fc35a3a8d882626
carlos0716 <chulezyc@gmail.com>
2023-07-18 19:29:23 -0600

    Problem solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+// const leapYears = function() {
 
-};
-
+// };
+function leapYears(n) {
+    if ((n % 100 === 0)&& (n % 400 !== 0)){
+      return false;
+    } else if (n % 4 === 0) {
+      return true;
+    }  else {
+      return false;
+    }
+   }
 // Do not edit below this line
 module.exports = leapYears;
